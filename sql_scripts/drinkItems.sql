c1728ad02d60dd26871565b34850b8f8f2d0fd53
Mike <michaela_us@tamu.edu>
2025-02-17 15:31:09 -0600

    added menu items

---
+++ sql_scripts/drinkItems.sql
@@ -0,0 +1,27 @@
+INSERT INTO Item (id, name, calories, price, sales)
+VALUES
+-- brewed tea
+(1, 'Classic Black Tea', 186, 3, 0),
+(2, 'Classic Green Tea', 186, 3, 0),
+(3, 'Classic Oolong Tea', 186, 3, 0),
+(4, 'Wintermelon Tea', 288, 3, 0),
+(5, 'Honey Tea (Black Tea)', 246, 3, 0),
+(6, 'Honey Tea (Green Tea)', 246, 3, 0),
+(7, 'Honey Tea (Oolong Tea)', 246, 3, 0),
+(8, 'Ginger Tea', 188, 3, 0),
+
+-- milk tea
+(9, 'Classic Milk Tea (Black Tea)', 469, 4, 0),
+(10, 'Classic Milk Tea (Green Tea)', 453, 4, 0),
+(11, 'Classic Milk Tea (Oolong Tea)', 453, 4, 0),
+(12, 'Honey Milk Tea (Black Tea)', 476, 4, 0),
+(13, 'Honey Milk Tea (Green Tea)', 476, 4, 0),
+(14, 'Honey Milk Tea (Oolong Tea)', 476, 4, 0),
+(15, 'Classic Coffee', 479, 4, 0),
+(16, 'Ginger Milk Tea', 410, 4, 0),
+(17, 'Coffee Milk Tea', 426, 4, 0),
+(18, 'Classic Pearl Milk Tea (Black)', 657, 5, 0),
+(19, 'Classic Pearl Milk Tea (Green Tea)', 646, 5, 0),
+(20, 'Classic Pearl Milk Tea (Oolong Tea)', 646, 5, 0);
+
+SELECT * FROM Item;
